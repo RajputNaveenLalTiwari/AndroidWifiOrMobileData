4975bcb610b93da79860817e490b9f3355d61367
unknown <2114@iapps-21.iaapps.local>
2017-06-29 10:09:30 +0530

    Initial commit

---
+++ app/src/main/java/com/example/workingonwifiormobiledata/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.workingonwifiormobiledata;
+
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (isOnline())
+        {
+            Toast.makeText(this,"connected to wifi",Toast.LENGTH_LONG).show();
+        }
+        else
+        {
+            Toast.makeText(this,"This app doesn't work without wifi",Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private boolean isOnline()
+    {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+        if (networkInfo!=null && networkInfo.isConnectedOrConnecting())
+        {
+            if (networkInfo.getType() != ConnectivityManager.TYPE_WIFI)
+            {
+                Toast.makeText(this,"This app doesnt work with wifi",Toast.LENGTH_LONG).show();
+                return false;
+            }
+            return true;
+        }
+        return false;
+    }
+}
